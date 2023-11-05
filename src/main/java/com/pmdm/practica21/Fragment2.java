51680ad11ce374fcc052101b9feac833f34894c6
Javier Lopez <javier.lopez.sola@gmail.com>
2023-11-05 16:36:41 +0100

    V0.4 - version funcional portrait y landscape

--- src/main/java/com/pmdm/practica21/Fragment2.java
+++ src/main/java/com/pmdm/practica21/Fragment2.java
@@ -1,13 +1,17 @@
 package com.pmdm.practica21;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentResultListener;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Fragment2 extends Fragment {
 
@@ -30,6 +34,15 @@ public class Fragment2 extends Fragment {
         if (bundle != null) {
             String suma = bundle.getString("agregar", "0");
             actualizarValorSuma(suma);
+        } {
+            getParentFragmentManager().setFragmentResultListener("suma", this, new FragmentResultListener() {
+                @Override
+                public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result) {
+                    String agregar = result.getString("agregar");
+                    Toast.makeText(getContext(),"fragment2 Agregar "+agregar, Toast.LENGTH_LONG).show();
+                    tv1.setText(agregar);
+                }
+            });
         }
         return view;
     }
--- src/main/java/com/pmdm/practica21/MainActivity.java
+++ src/main/java/com/pmdm/practica21/MainActivity.java
@@ -7,6 +7,17 @@ import android.content.res.Configuration;
 import android.os.Bundle;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
 
     private int suma;
